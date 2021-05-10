dc6cda2fc8044283dd3488631e4ae512edf87e3f
Mohamed Ashraf <mohamedelesaily0@gmail.com>
2021-05-10 06:49:57 +0200

    Updated drawer

--- backtracking/lib/Screens/drawer.dart
+++ backtracking/lib/Screens/drawer.dart
@@ -8,12 +8,61 @@ class AppDrawer extends StatelessWidget {
     return Drawer(
       child: Column(
         children: [
-          AppBar(
-            title: Text("BackTracking"),
-            automaticallyImplyLeading: false, // never display back buttom
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Theme.of(context).primaryColor,
+                  Colors.purple[200],
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+            padding: EdgeInsets.only(
+              top: 35,
+              left: 15,
+            ),
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 65,
+                  width: 65,
+                  child: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        "https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg"),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 12),
+                  child: Text(
+                    "Mohamed Ashraf",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 12),
+                  child: Text(
+                    "mohamedelesaily0@gmail.com",
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.grey[350],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-          Divider(),
+          SizedBox(
+            height: 10,
+          ),
           ListTile(
+            dense: true,
             leading: Icon(Icons.home),
             title: Text('Home'),
             onTap: () {
@@ -22,6 +71,7 @@ class AppDrawer extends StatelessWidget {
           ),
           Divider(),
           ListTile(
+              dense: true,
               leading: Icon(Icons.query_builder),
               title: Text('Production'),
               onTap: () {
@@ -30,30 +80,39 @@ class AppDrawer extends StatelessWidget {
               }),
           Divider(),
           ListTile(
+            dense: true,
             leading: Icon(Icons.store),
             title: Text('Stores'),
             onTap: () {},
           ),
           Divider(),
           ListTile(
-              leading: Icon(Icons.chat), title: Text('Chat'), onTap: () {}),
+            dense: true,
+            leading: Icon(Icons.chat),
+            title: Text('Chat'),
+            onTap: () {},
+          ),
           Divider(),
           ListTile(
+              dense: true,
               leading: Icon(Icons.shopping_cart),
               title: Text('Orders'),
               onTap: () {}),
           Divider(),
           ListTile(
+              dense: true,
               leading: Icon(Icons.person),
               title: Text('Clients'),
               onTap: () {}),
           Divider(),
           ListTile(
+              dense: true,
               leading: Icon(Icons.analytics),
               title: Text('Analysis'),
               onTap: () {}),
           Divider(),
           ListTile(
+            dense: true,
             leading: Icon(Icons.exit_to_app),
             title: Text('Logout'),
             onTap: () {},
