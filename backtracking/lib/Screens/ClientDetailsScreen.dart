c4bcbdc8ad4c76f988eb93bd56edeafa4fa6cd88
محمد اشرف بدوي موسي <36421064+mohamedmoussa22@users.noreply.github.com>
2021-07-15 15:07:02 +0200

    ClientDetailsScreen Done

--- backtracking/lib/Screens/ClientDetailsScreen.dart
+++ backtracking/lib/Screens/ClientDetailsScreen.dart
@@ -1,4 +1,5 @@
 import 'package:backtracking/Screens/Welcome/components/Button.dart';
+
 import 'package:backtracking/providers/customers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
@@ -27,7 +28,7 @@ class ClientDetailsScreen extends StatelessWidget {
               height: (size.height -
                       AppBar().preferredSize.height -
                       MediaQuery.of(context).padding.top) *
-                  0.3,
+                  0.4,
               width: double.infinity,
               color: Colors.white,
               child: SvgPicture.asset(
@@ -43,148 +44,125 @@ class ClientDetailsScreen extends StatelessWidget {
                         MediaQuery.of(context).padding.top) *
                     0.27,
               ),
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(75),
+                  topRight: Radius.circular(75),
+                ),
+              ),
               child: Container(
                 width: double.infinity,
-                padding: EdgeInsets.only(
-                  left: size.width * 0.04,
-                  top: size.height * 0.05,
-                ),
+                padding: EdgeInsets.symmetric(horizontal: size.width * 0.15),
                 child: Column(
-                    // crossAxisAlignment: CrossAxisAlignment.start,
                     mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Row(
-                        //mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
+                          Text(
+                            "Company Name:",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w200,
+                              fontSize: 15,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Text(
+                            customer.findCustomerById(customerId).company_name,
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          SizedBox(height: size.height * 0.02),
+                          Text(
+                            "Client Name:",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w200,
+                              fontSize: 15,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Text(
+                            customer.findCustomerById(customerId).customer_name,
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          SizedBox(height: size.height * 0.02),
+                          Text(
+                            "Email:",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w200,
+                              fontSize: 15,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Text(
+                            customer.findCustomerById(customerId).email,
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          SizedBox(height: size.height * 0.02),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.end,
                             children: [
-                              Text(
-                                "Company Name",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w100,
-                                  fontSize: 15,
-                                  color: Colors.white,
-                                ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "Phone:",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w200,
+                                      fontSize: 15,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  Text(
+                                    customer.findCustomerById(customerId).phone,
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                    ),
+                                  )
+                                ],
                               ),
-                              Text(
-                                customer
-                                    .findCustomerById(customerId)
-                                    .company_name,
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
+                              SizedBox(width: 10),
+                              GestureDetector(
+                                onTap: () {},
+                                child: Container(
+                                  padding: EdgeInsets.all(3),
+                                  decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.circular(30)),
+                                  child: Icon(
+                                    Icons.phone,
+                                    color: Colors.green,
+                                    size: 20,
+                                  ),
                                 ),
                               ),
                             ],
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "Client Name",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w100,
-                                  fontSize: 15,
-                                  color: Colors.white,
-                                ),
-                              ),
-                              Text(
-                                customer
-                                    .findCustomerById(customerId)
-                                    .customer_name,
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ],
-                          ),
+                          )
                         ],
                       ),
-                      Row(
-                        //mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "Email",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w100,
-                                  fontSize: 15,
-                                  color: Colors.white,
-                                ),
-                              ),
-                              Text(
-                                customer.findCustomerById(customerId).email,
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ],
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "Phone",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w100,
-                                  fontSize: 15,
-                                  color: Colors.white,
-                                ),
-                              ),
-                              Text(
-                                customer.findCustomerById(customerId).phone,
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-
-                      // Text(
-                      //   "Company Name: ${customer.findCustomerById(customerId).company_name}",
-                      //   style: TextStyle(fontSize: 20, color: Colors.white),
-                      // ),
-                      // SizedBox(
-                      //   height: size.height * 0.03,
-                      // ),
-                      // Text(
-                      //   "Client Name: ${customer.findCustomerById(customerId).customer_name}",
-                      //   style: TextStyle(fontSize: 20, color: Colors.white),
-                      // ),
-                      // SizedBox(
-                      //   height: size.height * 0.03,
-                      // ),
-                      // Text(
-                      //   "email: ${customer.findCustomerById(customerId).email}",
-                      //   style: TextStyle(fontSize: 20, color: Colors.white),
-                      // ),
-                      // SizedBox(
-                      //   height: size.height * 0.03,
-                      // ),
-                      // Text(
-                      //   "Phone: ${customer.findCustomerById(customerId).phone} \$",
-                      //   style: TextStyle(fontSize: 20, color: Colors.white),
-                      // ),
                       SizedBox(
-                        height: size.height * 0.03,
+                        height: size.height * 0.025,
                       ),
                       Center(
                         child: Button(
                           color: Colors.white,
                           press: () {},
-                          text: "CONTACT",
+                          text: "CONTACT ✉",
                           textColor: Theme.of(context).primaryColor,
                           width: size.width * 0.6,
                         ),
@@ -194,18 +172,11 @@ class ClientDetailsScreen extends StatelessWidget {
                           color: Color(0xFFF1E6FF),
                           press: () {},
                           text: "View Orders",
-                          textColor: Colors.black,
+                          textColor: Theme.of(context).primaryColor,
                           width: size.width * 0.6,
                         ),
                       ),
                     ]),
-                decoration: BoxDecoration(
-                  color: Theme.of(context).primaryColor,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(75),
-                    topRight: Radius.circular(75),
-                  ),
-                ),
               ),
             )
           ],
